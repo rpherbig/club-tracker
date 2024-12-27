f7d204f7e7715683c5026c5fdf85ca627f8b8c4c
Robert Herbig <sheltim@gmail.com>
2024-12-27 13:22:28 -0500

    Persist data to/from a local JSON file

--- club-tracker.js
+++ club-tracker.js
@@ -1,5 +1,8 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require('fs').promises;
 require('dotenv').config();
+
+const DATA_FILE = 'data.json';
 
 const client = new Client({
   intents: [
@@ -9,10 +12,29 @@ const client = new Client({
   ]
 });
 
-const clubEssence = new Map();
+async function loadEssence() {
+  try {
+    const data = await fs.readFile(DATA_FILE, 'utf8');
+    return new Map(Object.entries(JSON.parse(data)));
+  } catch {
+    console.log(`No data file found at: ${DATA_FILE}`);
+    return new Map();
+  }
+}
 
-client.on('ready', () => {
+async function saveEssence(essence) {
+  await fs.writeFile(
+    DATA_FILE,
+    JSON.stringify(Object.fromEntries(essence)),
+    'utf8'
+  );
+}
+
+let clubEssence;
+
+client.once('ready', async () => {
   console.log(`Logged in as ${client.user.tag}`);
+  clubEssence = await loadEssence();
 });
 
 client.on('interactionCreate', async interaction => {
@@ -23,6 +45,7 @@ client.on('interactionCreate', async interaction => {
       const amount = interaction.options.getInteger('amount');
       const username = interaction.user.username.toLowerCase();
       clubEssence.set(username, amount);
+      await saveEssence(clubEssence);
       await interaction.reply(`Set your essence to ${amount}`);
       break;
 
@@ -30,10 +53,12 @@ client.on('interactionCreate', async interaction => {
       const player = interaction.options.getString('player');
       const playerAmount = interaction.options.getInteger('amount');
       clubEssence.set(player.toLowerCase(), playerAmount);
+      await saveEssence(clubEssence);
       await interaction.reply(`Set ${player}'s essence to ${playerAmount}`);
       break;
 
     case 'show-essence':
+      //console.log(`interaction: ${Object.keys(interaction)}`)
       const targetPlayer = interaction.options.getString('player')?.toLowerCase() || interaction.user.username.toLowerCase();
       const essence = clubEssence.get(targetPlayer) || 0;
       await interaction.reply(`${targetPlayer} has ${essence} essence`);
